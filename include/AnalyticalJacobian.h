daf0f643364cfa3988b9462db8067c14c77218ba
sbreuils <breuils@nii.ac.jp>
2021-01-03 13:51:17 +0900

    Few updates

--- include/AnalyticalJacobian.h
+++ include/AnalyticalJacobian.h
@@ -1,8 +1,12 @@
-//
-// Created by Stéphane Breuils on 2021/01/03.
-//
 
 #ifndef DUALQUATICPAUTO_ANALYTICALJACOBIAN_H
 #define DUALQUATICPAUTO_ANALYTICALJACOBIAN_H
+
+
+void Compute_Jacobian(std::vector<Eigen::Triplet<float>>& coefficientsJ, float *Vertices, float *Normales, Eigen::VectorXf& residual, DualQuaternion * Warp, float * Weights, int *Indices, int * matches, int* MatchesList, int *Perm, float * VMap, InputFrame *Frame, int nb_N, int nb_V, int nb_matches, int fact) {
+    //float* VMap = Frame->getVMap();
+
+}
+
 
 #endif //DUALQUATICPAUTO_ANALYTICALJACOBIAN_H
